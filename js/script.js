d98cf4c7d2140a4294a0d0fec3e44c8179ed7130
brian-nyabuto12 <brian.nyabuto@student.moringaschool.com>
2021-04-29 17:01:09 +0300

    replace codes

--- js/script.js
+++ js/script.js
@@ -1,11 +1,132 @@
 $(document).ready(function(){
-    $("div.hide").hide();
-
-$("h3").click(function () {
-    $(this).next("div.hide").slideToggle("slow");
-});
-
-$("button.hide").click(function () {
-    $(this).parents("div.hide").slideToggle("slow");
-});
-});
+    $(".text1").click(function(){
+      $("#image1").show(1000);
+      $(".text1").hide(1000);
+    });
+    $("#image1").click(function(){
+      $(".text1").show(1000);
+      $("#image1").hide(1000);
+    });
+   
+  });
+  $(document).ready(function(){
+    $(".text1").click(function(){
+      $("#image2").show(1000);
+      $(".text1").hide(1000);
+    });
+    $("#image2").click(function(){
+      $(".text1").show(1000);
+      $("#image2").hide(1000);
+    });
+   
+  });
+  $(document).ready(function(){
+    $(".text1").click(function(){
+      $("#image3").show(1000);
+      $(".text1").hide(1000);
+    });
+    $("#image3").click(function(){
+      $(".text1").show(1000);
+      $("#image3").hide(1000);
+    });
+   
+  });
+  $(document).ready(function(){
+    $(".text1").click(function(){
+      $("#image4").show(1000);
+      $(".text1").hide(1000);
+    });
+    $("#image4").click(function(){
+      $(".text1").show(1000);
+      $("#image4").hide(1000);
+    });
+   
+  });
+  $(document).ready(function(){
+    $(".text1").click(function(){
+      $("#image5").show(1000);
+      $(".text1").hide(1000);
+    });
+    $("#image5").click(function(){
+      $(".text1").show(1000);
+      $("#image5").hide(1000);
+    });
+   
+  });
+  $(document).ready(function(){
+    $(".text1").click(function(){
+      $("#image6").show(1000);
+      $(".text1").hide(1000);
+    });
+    $("#image6").click(function(){
+      $(".text1").show(1000);
+      $("#image6").hide(1000);
+    });
+   
+  });
+  $(document).ready(function(){
+    $(".text1").click(function(){
+      $("#image7").show(1000);
+      $(".text1").hide(1000);
+    });
+    $("#image7").click(function(){
+      $(".text1").show(1000);
+      $("#image7").hide(1000);
+    });
+   
+  });
+  $(document).ready(function(){
+    $(".text1").click(function(){
+      $("#image8").show(1000);
+      $(".text1").hide(1000);
+    });
+    $("#image8").click(function(){
+      $(".text1").show(1000);
+      $("#image8").hide(1000);
+    });
+   
+  });
+  $(document).ready(function(){
+    $(".text1").click(function(){
+      $("#image9").show(1000);
+      $(".text1").hide(1000);
+    });
+    $("#image9").click(function(){
+      $(".text1").show(1000);
+      $("#image9").hide(1000);
+    });
+   
+  });
+  $(document).ready(function(){
+    $(".text1").click(function(){
+      $("#image10").show(1000);
+      $(".text1").hide(1000);
+    });
+    $("#image10").click(function(){
+      $(".text1").show(1000);
+      $("#image10").hide(1000);
+    });
+   
+  });
+  $(document).ready(function(){
+    $(".text1").click(function(){
+      $("#image11").show(1000);
+      $(".text1").hide(1000);
+    });
+    $("#image11").click(function(){
+      $(".text1").show(1000);
+      $("#image11").hide(1000);
+    });
+   
+  });
+  $(document).ready(function(){
+    $(".text1").click(function(){
+      $("#image12").show(1000);
+      $(".text1").hide(1000);
+    });
+    $("#image12").click(function(){
+      $(".text1").show(1000);
+      $("#image12").hide(1000);
+    });
+   
+  });
